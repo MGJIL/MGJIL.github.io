4af92687d4b153809ca0aee992f5717d1d5c03c8
ARKANYOTA <lesarktime@gmail.com>
2021-03-12 18:25:48 +0100

    Positiondes des images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,92 @@
-import logo from './logo.svg';
+import Pot from './image/PotRez.svg';
+import PipesRight from './image/piperight.svg';
+import PipesLeft from './image/pipeleft.svg';
+import GameJam from './image/Gamejam.svg';
 import './App.css';
+import React from 'react';
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <div class="Menu">
+        <img class="pot" src={Pot} alt="Pot"/>
+        <img class="piperight" src={PipesRight} alt="Pot"/>
+        <img class="pipeleft" src={PipesLeft} alt="Pot"/>
+        <div class="gamejamdiv" ><img class="gamejam" src={GameJam} alt="Pot"/></div>
+      </div>
+    );
+  }
+}
+class Welcome extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+class WhatIs extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+class WhoAre extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}/*
+
+class WhatDo extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+class Conclusion extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello </h1>
+      </React.Fragment>
+    );
+  }
+}*/
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <Menu/>
+        <Welcome/>
+        <WhatIs/>
+        <WhoAre/>
+
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+        <WhatDo/>
+        <Conclusion/>
+*/
+/*<!--
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,10 +98,4 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        </a>-->*/
